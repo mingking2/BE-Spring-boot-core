8adb67ce6c8e50bef6a90c90634a7533560d5281
King's meow <mingee641@gmail.com>
2023-08-03 20:58:58 +0900

    7. 의존관계 자동 주입 - @Autowired 필드 명, @Qualifier, @Primary

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -21,7 +21,7 @@ class RateDiscountPolicyTest {
         //when
         int discount = discountPolicy.discount(member, 10000);
         //then
-        assertThat(discount).isEqualTo(1000);
+        assertThat(discount).isEqualTo(2000);
     }
 
     @Test
