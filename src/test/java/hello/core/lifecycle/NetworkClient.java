9aca20b1c242f9f115f97315c8e758d508f09292
King's meow <mingee641@gmail.com>
2023-08-05 10:31:07 +0900

    8. 빈 생명주기 콜백 - 강의3개 완료

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,15 @@
 package hello.core.lifecycle;
 
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
-
     }
 
     public void setUrl(String url) {
@@ -28,4 +29,19 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("close: " + url);
     }
+
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
 }
